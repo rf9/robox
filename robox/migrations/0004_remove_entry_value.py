1bc5d48a113402a5a3ebb11e6fc549bcd399eb3e
Ronan Forman <rf9@sanger.ac.uk>
2015-09-03 11:08:19 +0100

    Removed value field from entry table.

---
+++ robox/migrations/0004_remove_entry_value.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('robox', '0003_file_format'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='entry',
+            name='value',
+        ),
+    ]
